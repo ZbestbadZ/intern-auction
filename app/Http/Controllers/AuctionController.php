fab0ea79375842f343231b29c907002b9c416d82
relaxBear20 <long061020@gmail.com>
2020-09-03 11:29:54 +0700

    Update AuctionController

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuctionUpdateRequest;
-use Illuminate\Http\Request;
 use App\Models\Auction;
+use Carbon\Carbon;
 use Exception;
 
 class AuctionController extends Controller
@@ -16,15 +16,41 @@ class AuctionController extends Controller
         $this->auctionModel = $model;
     }
 
-    public function update(AuctionUpdateRequest $request) {
-        try{
-            $data = $request->only('start_date','end_date');
-        
+    public function update(AuctionUpdateRequest $request)
+    {
+        try {
             $model = $this->auctionModel->find($request->id);
+
+            $data = $request->only('end_date', );
+
+            if ($data['end_date'] !== $model->end_date) {
+                $data['start_date'] = Carbon::now();
+            }
+
+            $mode = $request['mode'];
+
+            switch ($mode) {
+                case 'restart':{
+                        $product = $model->product;
+                        $product->update(['is_bidding'=>'1']);
+                        
+                        break;
+                    }
+                case 'update':{
+                        
+                    }
+                case 'start':{
+                        $product = $model->product;
+                        $product->update(['is_bidding'=>'1']);
+                    }
+            }
+
             $model->update($data);
-            return  redirect()->back()->withInput();
-        } catch(Exception $e) {
-            $mess = $e->getMessage() ;
+            return redirect()->back()->withInput();
+            dd($request['restart']);
+
+        } catch (Exception $e) {
+            $mess = $e->getMessage();
             return redirect()->route('products.index')->withErrors($mess)->withInput();
         }
     }
