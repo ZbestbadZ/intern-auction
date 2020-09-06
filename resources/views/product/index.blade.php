77b7e18810a9778828c7c6e10c5dff55c81656e2
hoangph2 <hoangph2@miichisoft.com>
2020-09-06 10:04:52 +0700

    Add default image for product

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -17,7 +17,7 @@
                             @if (count($item->images) === 0)
                                 <a href="products/{{ $item->id }}">
                                     <img class="img-fluid" style="height: 150px"
-                                        src="{{URL::asset('/storage/uploads/defaultProduct.jpg')}}"
+                                        src="{{URL::asset('/img/defaultProductImage.jpg')}}"
                                         alt="">
                                 </a>
                             @else
